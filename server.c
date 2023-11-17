9ae75c0ddac688cdb1594ac7fcb5a26cf193c177
qianxiaolin <2722585081@qq.com>
2023-11-17 23:30:04 +0800

    create-server

--- server.c
+++ server.c
@@ -1,16 +1,15 @@
 #include<stdio.h>
-
-struct client_st{
-	int fd;
-	char msg[MSGSIZE];
-}
-struct server_st{
-	int socket;/*server socket file */
+#define MAXCLIENTS 1024 
+struct client_st{ 
+	int fd; 
+	char msg[MSGSIZE]; 
+} 
+struct server_st{ int socket;/*server socket file */
 	int clientnums;
 	strcut client_st* clients[MAXCLIENT];
 }
 
-struct server_st server;
+struct server_st *server;
 
 void create_client(fd){
 	struct client_st* client=malloc();
@@ -19,26 +18,30 @@ void create_client(fd){
 	return 1;
 
 }
-
-void server_init(){
+void create_tcp_server(){
 	struct sockfd;
 	struct sockaddr_in server_addr;
 	server_addr.sin_family;
 	server_addr.sin_port=;
 	sockfd=socket();
-	server->fd=fd;
+	if(socketfd==-1){
+	}
 	bind();
-	server->clientnum=0;
-		
+	return sockfd;
+}
+void server_init(){
+	memset(server,0,sizeof(*server));
+	create_tcp_server();	
+	server->clientnum=0;	
 }
 int main(){
 	server_init();	
-
-
+	int readset;
+	FD_
+	select();
 	while(1){
-	fd=accept();
-	create_client(fd);
-	
+		fd=accept();
+		create_client(fd);
 	}
 
 	return 0;
