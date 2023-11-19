8f9654d3e86981396576828c1a9d35ea0036fba4
qianxiaolin <2722585081@qq.com>
2023-11-19 16:52:13 +0800

    hello

--- server.c
+++ server.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 #define MAXCLIENTS 1024 
+#define BUFFSIZE 1024
 struct client_st{ 
 	int fd; 
 	char msg[MSGSIZE]; 
@@ -12,59 +13,85 @@ struct server_st{
 
 struct server_st *server;
 
-void create_client(fd){
-	struct client_st* client=malloc();
+void* create_client(int fd){
+	struct client_st* client=(struct server_st*)malloc(sizeof(struct server_st));
+        if(client==NULL){
+                return NULL;
+        }
 	clients[++clientnums]=client;
 	client->fd=fd;
-	return 1;
-
+	return client;
 }
 void create_tcp_server(){
-	struct sockfd;
+	int sockfd;
 	struct sockaddr_in server_addr;
-	server_addr.sin_family;
-	server_addr.sin_port=;
-	sockfd=socket();
+	server_addr.sin_family=AF_INET;
+	server_addr.sin_port=htons(1989);
+        inet_pton(AF_INET,"127.0.0.1",server_addr.sin_addr);
+	sockfd=socket(AF_INET,&server_addr,sizeof(server_addr));
 	if(socketfd==-1){
+                perror("socket");
+                exit(1);
 	}
-	bind();
+        socklen_t len=sizeof(server_addr);
+	bind(socketfd,server_addr.sin_addr,len);
         server->sockfd=sockfd;
+        return sockfd;
 }
 void server_init(){
-	memset(server,0,sizeof(*server));
-	create_tcp_server();	
+	
+	server->sockfd=create_tcp_server();	
+        memset(server,0,sizeof(*server));
 	server->clientnum=0;	
+        
 }
-int main(){
-	server_init();	
-	int readset;
-	FD_SERO();
-        FD_();
-        for(int i=0;i<server->clientnum;i++){
-                if(server->client[i]){
-                        int fd=server->client[i]->fd; 
-                        FD_();
-                }
-        }
-	int res=select();
-        if(res<0){
-        }
-        else{
-                if(FD_(server->sockfd,readset)){
-                        accept_client();
-                        create_client();
+void read_msg(int fd){
+        char buff[BUFFSIZE];
 
-                }
+        read(fd,buff,BUFFSIZE);
+        puts(buff);
+}
+void accept_client(int fd){
+        struct sockaddr_in client_addr;
+        accept(fd,&client_addr,sizeof(client_addr));
+        sprintf(stdout,"IP为%s的用户已加入聊天");
+
+}
+int main(int argc,char *argv[]){
+	server_init();	
+        while(1){
+                fd_set readset;
+                FD_ZERO(&readset);
+                FD_SET(server->sockfd,&readset)
                 for(int i=0;i<server->clientnum;i++){
                         if(server->client[i]){
-                                if(FD_SET(server->client[i]->fd,readset)){
-                                        read_msg(); 
-
-                                }
-                                
+                                int fd=server->client[i]->fd; 
+                                FD_SET(fd,&readset);
+                        }
+                }
+                struct timeval tv;
+                tv.tv_sec = 1; // 1 sec timeout
+                tv.tv_usec = 0;
+                int maxfd;
+                if(server->clientnums<MAXCLIENT){
+                        maxfd=clientnums+1;
+                }
+                int res=select(maxfd+1,readset,NULL,NULL,$tv);
+                if(res<0){
+                        perror("select() error");
+                        exit(1);
+                }
+                if(FD_ISSET(server->sockfd,readset)){
+                                accept_client();
+                                create_client();
+                }
+                for(int i=0;i<server->clientnum;i++){
+                        if(server->client[i]&&FD_ISSET(server->client[i]->fd,readset)){
+                                read_msg(server->client[i]->fd); 
                         }
                 }
         }
+	
 
 	return 0;
 }
