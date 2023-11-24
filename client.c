c6deb1b449217849e4c5381035f8d5ea05de07ed
qianxiaolin <2722585081@qq.com>
2023-11-24 10:42:16 +0800

    send_message

--- client.c
+++ client.c
@@ -1,16 +1,19 @@
 #include<stdio.h>
 #include<sys/socket.h>
 
-
+/*socket*/
 void socket_init(const char *ip,const char * port){
 	int fd;
 	struct sockaddr_in client_addr;
 	client_addr.sin_family=AF_INET;
 	client_addr.sin_port=htons(atoi(ip));
 	inet_pton(AF_INET,ip,client_addr.sin_ip);
-
 	fd=socket(AF_INET,&client_addr,sizeof(client_addr));
-	bind();	
+	if(fd<0){
+	
+	
+	}
+	bind(fd,&client_addr,sizeof(client_addr));	
 
 }
 void connect_server(){
@@ -38,11 +41,11 @@ void receive_msg(int fd){
 	write(stdout,buff,BUFFSIZE);	
 }
 void send_msg(int fd){
-	sprintf(stdout,"input message");
+	sprintf(stdout,"input message:");
 	char input[1024];
-	gets();
-	fopen();
-	write();
+	input=gets();
+	write(fd,input,sizeof(input));
+	close(fd);
 }
 int main(int argc,char *argv[]){
 	if(argc<3){
