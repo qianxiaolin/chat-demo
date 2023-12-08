54dd741ed11bd0b3dca6ba14e85e4a18402fbb35
qianxiaolin <2722585081@qq.com>
2023-12-08 22:53:21 +0800

    make color.c

---
+++ color/color.c
@@ -0,0 +1,12 @@
+/*
+ * 	file:color.c
+ * 	description:lib to make print with different color and position
+ * 	author:qianxiao
+ * 	date:2023/12/8
+ */
+#include<stdio.h>
+#include"color.h"
+int main(){
+
+	return 0;
+}
