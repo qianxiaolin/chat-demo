4aacc3e91be22a13a62048d12c62763346bcad15
qianxiaolin <2722585081@qq.com>
2023-12-09 20:33:29 +0800

    print

--- color/color.c
+++ color/color.c
@@ -5,8 +5,25 @@
  * 	date:2023/12/8
  */
 #include<stdio.h>
-#include"color.h"
-int main(){
+#define RED "\033[0;31m"
+#define GREEN "\033[0;32m"
+#define LIGHT_GREEN "\033[1;32m"
+#define YELLOW "\033[1;33m"
+#define BLUE "\033[0;34m"
+#define WHITE "\033[0;37m"
 
+typedef const char* Color;
+void clean_screen(){
+}
+void clean_line(){
+}
+#define  print_color_text(color,str)\
+	fprintf(stdout,"%s%s\n",(color),(str));
+int main(){
+	print_color_text(RED,"hello");
+	print_color_text(LIGHT_GREEN,"hello");
+	print_color_text(GREEN,"hello");
+	print_color_text(YELLOW,"hello");
+	print_color_text(BLUE,"hello");
 	return 0;
 }
